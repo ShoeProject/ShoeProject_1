3d2f4760034c9fa1e8ec37f4aee8bd5ad84ed5f0
lakshmadu <lakshmadu2000@gmail.com>
2022-11-05 16:35:48 +0530

    aded features

--- ClientApp/shared/header.php
+++ ClientApp/shared/header.php
@@ -1,13 +1,4 @@
 
-<head>
-<title>Login Page</title>
-    <link href="../vendor/twbs/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="../vendor/twbs/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet" />
-    <link rel="stylesheet" href="asset/css/style.css" />
-    <script src="../vendor/twbs/bootstrap/dist/js/bootstrap.bundle.js"></script>
-    <script src="../vendor/components/jquery.slim.min.js"></script>
-    <script src="asset/js/main.js"></script>
-</head>
 
 <nav class="nav flex-row-reverse gap-3 bd-light">
   <a class="nav-link active" aria-current="page" href="#">
@@ -16,15 +7,15 @@
   </a>
   <a class="nav-link text-dark" href="#">Admin</a>
   <div class="vr mt-1 text-dark"></div>
-  
+
   <div class="dropdown">
-  <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
-    <i class="bi bi-bell text-dark"></i>
-  </a>
-  <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-    <li><a class="dropdown-item" href="#">Action</a></li>
-    <li><a class="dropdown-item" href="#">Another action</a></li>
-    <li><a class="dropdown-item" href="#">Something else here</a></li>
-  </ul>
-</div>
+    <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+      <i class="bi bi-bell text-dark"></i>
+    </a>
+    <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+      <li><a class="dropdown-item" href="#">Action</a></li>
+      <li><a class="dropdown-item" href="#">Another action</a></li>
+      <li><a class="dropdown-item" href="#">Something else here</a></li>
+    </ul>
+  </div>
 </nav>
