8693b7e8eb2f36e808e4076527e6529a015fd3b8
lakshmadu <lakshmadu2000@gmail.com>
2022-12-28 14:42:38 +0530

    test

--- ClientApp/feature/sidebar.php
+++ ClientApp/feature/sidebar.php
@@ -15,12 +15,12 @@
 
 		.sidenavbar {
 			background:#2f323A;
-            margin-top: 0px;
-            padding-top: 20px;
+            /* margin-top: 0px;
+            padding-top: 20px; */
            
-            left:0;
+            /* left:0;
             width:250px;
-            height:100%;
+            height:100%; */
 
 
 		}
@@ -28,9 +28,9 @@
 	</style>
 
 <body>
-    <div class="sidenavbar" text-white>
-    <div class="col-auto col-3 col-2 px-2 px-0 bg-highlight">
-        <div class="d-flex flex-column align-items-center align-items-sm-start px-1 pt-2 min-vh-100">
+    <!-- <div class="container-fluid sidenavbar" text-white> -->
+    <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-highlight sidenavbar">
+        <div class="d-flex flex-column align-items-center align-items-sm-start px-1 pt-2 text-dark min-vh-100">
             <!-- <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
                         <span class="fs-5 d-none d-sm-inline">Menu</span>
                     </a> -->
@@ -76,7 +76,7 @@
             <hr>
         </div>
     </div>
-    </div>
+    <!-- </div> -->
 </body>
 
 </html>
